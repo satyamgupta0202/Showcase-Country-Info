db3763fcae2095244e4ecc03b8ce773f4d659b1f
satyam <2019335@iiitdmj.ac.in>
2021-06-13 08:04:48 +0530

    Country+Neighbour Configured

--- script.js
+++ script.js
@@ -4,17 +4,9 @@ const btn = document.querySelector('.btn-country');
 const countriesContainer = document.querySelector('.countries');
 
 ///////////////////////////////////////
-const getcountry = function (country) {
-  const request = new XMLHttpRequest();
-  request.open('GET', `https://restcountries.eu/rest/v2/name/${country}`);
-  request.send();
-
-  request.addEventListener('load', function () {
-    console.log(this.responseText);
-    const [data] = JSON.parse(this.responseText);
-    console.log(data);
-    const html = `
-  <article class="country">
+const renderCountry = function (data, className = '') {
+  const html = `
+<article class="country ${className}">
           <img class="country__img" src="${data.flag}" />
           <div class="country__data">
             <h3 class="country__name">${data.name}</h3>
@@ -24,12 +16,31 @@ const getcountry = function (country) {
             <p class="country__row"><span>💰</span>${data.currencies[0].name}</p>
           </div>
         </article>
-  `;
-    countriesContainer.insertAdjacentHTML('beforeend', html);
-    countriesContainer.style.opacity = 1;
+        `;
+  countriesContainer.insertAdjacentHTML('beforeend', html);
+  countriesContainer.style.opacity = 1;
+};
+const getcountry = function (country) {
+  const request = new XMLHttpRequest();
+  request.open('GET', `https://restcountries.eu/rest/v2/name/${country}`);
+  request.send();
+
+  request.addEventListener('load', function () {
+    // console.log(this.responseText);
+    const [data] = JSON.parse(this.responseText);
+    // console.log(data);
+    renderCountry(data);
+    //2
+    const [neighbour] = data.borders;
+    if (!neighbour) return;
+    const request2 = new XMLHttpRequest();
+    request2.open('GET', `https://restcountries.eu/rest/v2/alpha/${neighbour}`);
+    request2.send();
+    request2.addEventListener('load', function () {
+      const data = JSON.parse(this.responseText);
+      renderCountry(data, 'neighbour');
+    });
   });
 };
 
-getcountry('Portugal');
-getcountry('India');
-getcountry('japan');
+getcountry('usa');
