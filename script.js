194c6c9df0e41cb747a6b241c1912f022a013a1f
Satyam Gupta <58383389+satyamgupta0202@users.noreply.github.com>
2021-09-20 18:34:55 +0530

    Update script.js

--- script.js
+++ script.js
@@ -56,7 +56,7 @@ const getCountry = function (country) {
 };
 
 btn.addEventListener('click', function () {
-  getCountry('hiuhguyggguyg');
+  getCountry('portugal');
 });
 
 ////challenge!!!!!!!!!!!!!!!!!!!!
